5a6b0d1d5bd7c44d1aad8a767767747d26f45896
Nicolas Derobert <derobert.pro@gmail.com>
2022-04-21 16:29:01 +0200

    redux installed

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,17 +1,23 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
 import "./Login.css";
 
 function Login(props) {
-	const [enteredTitle, setEnteredTitle] = useState("");
-	const [enteredAmount, setEnteredAmount] = useState("");
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
+	const [isLogin, setIsLogin] = useState(true);
+	// const [enteredTitle, setEnteredTitle] = useState("");
+	// const [enteredAmount, setEnteredAmount] = useState("");
 	// const [userInput, setUserInput] = useState({
 	//   enteredTitle: '',
 	//   enteredAmount: '',
 	//   enteredDate: '',
 	// });
-
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState);
+	  };
+	
 	const titleChangeHandler = (event) => {
-		setEnteredTitle(event.target.value);
+		// setEnteredTitle(event.target.value);
 		// setUserInput({
 		//   ...userInput,
 		//   enteredTitle: event.target.value,
@@ -22,7 +28,7 @@ function Login(props) {
 	};
 
 	const amountChangeHandler = (event) => {
-		setEnteredAmount(event.target.value);
+		// setEnteredAmount(event.target.value);
 		// setUserInput({
 		//   ...userInput,
 		//   enteredAmount: event.target.value,
@@ -31,16 +37,38 @@ function Login(props) {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
+		const enteredEmail = emailInputRef.current.value;
+		const enteredPassword = passwordInputRef.current.value;
 
-		const expenseData = {
-			title: enteredTitle,
-			amount: enteredAmount,
-		};
+		// props.onSaveExpenseData(expenseData);
 
-		props.onSaveExpenseData(expenseData);
-		setEnteredTitle("");
-		setEnteredAmount("");
-	};
+		if (isLogin) {
+		} else {
+		  fetch(
+			'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBZhsabDexE9BhcJbGxnZ4DiRlrCN9xe24',
+			{
+			  method: 'POST',
+			  body: JSON.stringify({
+				email: enteredEmail,
+				password: enteredPassword,
+				returnSecureToken: true,
+			  }),
+			  headers: {
+				'Content-Type': 'application/json',
+			  },
+			}
+		  ).then((res) => {
+			if (res.ok) {
+			  // ...
+			} else {
+			  return res.json().then((data) => {
+				// show an error modal
+				console.log(data);
+			  });
+			}
+		  });
+		}
+	  };
 	return (
 		<Fragment>
 			<div className="bg-dark">
@@ -54,17 +82,20 @@ function Login(props) {
 								<input
 									type="text"
 									id="username"
-									value={enteredTitle}
-									onChange={titleChangeHandler}
+									// value={enteredTitle}
+									// onChange={titleChangeHandler}
+									required
+									ref={emailInputRef}
 								/>
 							</div>
 							<div className="input-wrapper">
-								<label for="password">Amount</label>
+								<label for="password">Password</label>
 								<input
 									type="text"
 									id="password"
-									value={enteredAmount}
-									onChange={amountChangeHandler}
+									// value={enteredAmount}
+									// onChange={amountChangeHandler}
+									ref={passwordInputRef}
 								/>
 							</div>{" "}
 							<div class="input-remember">
@@ -72,7 +103,7 @@ function Login(props) {
 								<input
 									type="checkbox"
 									id="remember-me"
-									value={enteredAmount}
+									// value={enteredAmount}
 								/>
 							</div>
 						</div>
